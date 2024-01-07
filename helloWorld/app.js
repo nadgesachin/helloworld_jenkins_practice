62bcbf303e60d9a7c75e961f8aa376b9a7ba9e82
pat nadge <pat@pats-MacBook-Air.local>
2024-01-07 15:13:21 +0530

    initial commit

---
+++ helloWorld/app.js
@@ -0,0 +1,29 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+    // console.log(req);
+    // console.log(req.headers);
+
+    // const log = `${Date.now()} : ${req.url} New Req Received ${req.headers.host}\n`;
+    // // append request details on file
+    // fs.appendFile('log.txt', log, (err, data) => {
+    //     if (err) {
+    //         console.log("sothing went wrong");
+    //     } else {
+    //         console.log(data);
+    //     } 
+    // }); 
+
+    switch (req.url) {
+        case '/': res.end("Hello Home Page");
+            break;
+        case '/about': res.end("About Page");
+            break;
+        default: res.end("page not found");
+    }
+});
+
+server.listen(8000,() => {
+    console.log("server start at 8000");
+});
